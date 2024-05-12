10d4330b47e56a172871ad9265a68161a2d2232a
qGdala <114633819+qGdala@users.noreply.github.com>
2024-05-12 08:26:43 +0800

    Update script.js

--- test1/script.js
+++ test1/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const dessertsSection = document.getElementById("desserts");
 
     const desserts = [
-        { name: "<span style='color: #D2B48C;'> 旺萊桂花韻 </span>", price: " $ 1360", image: "../test1/cakebase/OsmanthusOolong.png", description: "-⋆- ~!@#$%^&*()_+<br>-⋆- a!@#$%^&*()_+<br>-⋆- b!@#$%^&*()_+<br>-⋆- c!@#$%^&*()_+" },
+        { name: "<span style='color: #D2B48C;'> 旺萊桂花韻 </span>", price: " $ 1360", image: "../../test1/cakebase/OsmanthusOolong.png", description: "-⋆- ~!@#$%^&*()_+<br>-⋆- a!@#$%^&*()_+<br>-⋆- b!@#$%^&*()_+<br>-⋆- c!@#$%^&*()_+" },
         { name: "<span style='color: #8B4513;'> Mr.伯爵 </span>", price: " $ 1460 ", image: "../test1/cakebase/MrCount.png", description: "-⋆-  伯爵戚風<br>-⋆-  伯爵奶凍<br>-⋆-  手熬伯爵奶醬<br>-⋆-  日本蜜柑<br>-⋆-  伯爵香堤" },
         { name: "<span style='color: #503225;'> 可可貴妃 </span>", price: " $ 1560 ", image: "../test1/cakebase/CocoaImperialConcubine.png", description: "-⋆-  法芙娜可可戚風<br>-⋆-  巧克力慕斯<br>-⋆-  新鮮草莓<br>-⋆-  巧克力香堤夾餡<br>-⋆-  原味香堤抹面" },
     ];
